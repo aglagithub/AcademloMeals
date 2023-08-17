6a89311750eb90d0ebb23fcdd82e73eac7eb4b9d
Andres Lara <angillama@gmail.com>
2023-08-17 12:09:03 -0500

    First Commit

---
+++ NodeInstallarions.bat
@@ -0,0 +1,13 @@
+npm i nodemon --save-dev
+npm i morgan --save-dev
+npm i sequelize
+npm i pg pg-hstore
+npm i dotenv
+npm i cors
+npm i express
+npm i express-validator
+npm i bcryptjs
+npm i jsonwebtoken
+
+
+ 
